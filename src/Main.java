fed5fedecf535023705441b618d6670401be2e7f
vestgar <sid06@bk.ru>
2025-04-17 23:37:44 +0300

    modification

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите сумму пополнения");
+        int input = in.nextInt();
         int usersAccount = 100;
-        int amount = 9000;
-        int bonus = amount / 100;
+        int bonus = input / 100 * 3;
+        int increasedBonus = input / 100 * 6;
+        int maximumBonus = input / 100 * 12;
+        int commis = input / 100 * 1;
 
-        if (amount > 1000) {
-            System.out.println("Сумма на счете: " + (usersAccount + amount + bonus));
-
-        } else {
-            System.out.println("Сумма на счете: " + (usersAccount + amount));
+        if (input <= 0) {
+            System.out.println("Не верная сумма пополнения! Введите верную сумму!");
         }
-
-
+        if (input > 0 && input < 100) {
+            System.out.println("Минимальная сумма пополнения 100 рублей!");
+        }
+        if (input >= 100 && input <= 1000) {
+            System.out.println("Спасибо за пополнения счета!");
+            System.out.println("Сумма на счете: " + (input + usersAccount - commis));
+            System.out.println("Сумма бонусов: " + (0) + " руб.");
+            System.out.println("Комиссия составит: " + (commis) + " руб.");
+            System.out.println("Будем рады видеть вас снова!");
+        }
+        if (input > 1000 && input <= 2000) {
+            System.out.println("Спасибо за пополнения счета!");
+            System.out.println("Сумма на счете: " + (input + bonus + usersAccount - commis));
+            System.out.println("Сумма бонусов: " + (bonus) + " руб.");
+            System.out.println("Комиссия составит: " + (commis) + " руб.");
+            System.out.println("Будем рады видеть вас снова!");
+        }
+        if (input > 2000 && input <= 5000) {
+            System.out.println("Спасибо за пополнения счета!");
+            System.out.println("Сумма на счете: " + (input + increasedBonus + usersAccount - commis));
+            System.out.println("Сумма бонусов: " + (increasedBonus) + " руб.");
+            System.out.println("Комиссия составит: " + (commis) + " руб.");
+            System.out.println("Будем рады видеть вас снова!");
+        }
+        if (input > 5000 && input <= 10000) {
+            System.out.println("Спасибо за пополнения счета!");
+            System.out.println("Сумма на счете: " + (input + maximumBonus + usersAccount - commis));
+            System.out.println("Сумма бонусов: " + (maximumBonus) + " руб.");
+            System.out.println("Комиссия составит: " + (commis) + " руб.");
+            System.out.println("Будем рады видеть вас снова!");
+        }
+        if (input > 10000) {
+            System.out.println("Превышен лимит пополнения");
+        }
     }
 }
